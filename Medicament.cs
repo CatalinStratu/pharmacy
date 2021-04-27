354d62cb2e3cc2f683025b0e993617f617042376
Catalin Stratu <catalinstratu45@gmail.com>
2021-04-28 01:37:42 +0300

    first commit

---
+++ Medicament.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Medicament
+    {
+        private string nume;
+        int nrstocuri;
+        int nrcomprimate;
+        
+        public Medicament(string sir)
+        {
+            string[] date = sir.Split(',');
+            this.nume = date[0];
+            this.nrstocuri = Convert.ToInt32(date[1]);
+            this.nrcomprimate = Convert.ToInt32(date[2]);
+        }
+
+        public string conversieLaSir()
+        {
+            return $" Nume:{nume}, Nrstocuri: {nrstocuri}, NrComprimate: {nrcomprimate}";
+        }
+
+        public string compare(Medicament a, Medicament b)
+        {
+            if(a.nrcomprimate > b.nrcomprimate)
+            {
+                return $"Primul medicament e mai mare";
+            } else if (a.nrcomprimate < b.nrcomprimate){
+                return $"Al doilea medicament e mai mare";
+            }
+            return "Medicamentele au aceleasi numar de comprimante";
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu();
+            }
+        }
+        private static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Adaugare medicament");
+            Console.WriteLine("2) Cautare");
+            Console.WriteLine("3) Exit");
+            Console.Write("\r\nSelect an option: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.Write("Catalin.");
+                    return true;
+                case "2":
+                    return true;
+                case "3":
+                    return false;
+                default:
+                    return true;
+            }
+        }
+    }
+}
